e018c2519b550965eeec70b7ed2fda2467bacbe2
HomereAMK <hmon@aqua.dtu.dk>
2022-02-16 19:27:49 -0500

    backup

--- 04_local_R/00_scripts/BSG_FlatOyster--Fst.R
+++ 04_local_R/00_scripts/BSG_FlatOyster--Fst.R
@@ -14,7 +14,8 @@ pacman::p_load(pheatmap, tidyverse, reshape2)
 
 
 # Loads Fst table ~
-Fst <- read.table("Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst.tsv", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+#Fst <- read.table("~/Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst.tsv", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+Fst <- read.table("~/Desktop/Scripts/Data/FST/8Feb22_5pop_NOLIST--Fst.tsv", sep = "\t", header = FALSE, stringsAsFactors = FALSE)
 
 
 # Adds column names ~
@@ -34,7 +35,8 @@ for (i in 1:nrow(Fst)) {
 
 
 # Writes Fst-Sites matrix ~
-write.table(FstSites, "Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst-Sites.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+#write.table(FstSites, "~/Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst-Sites.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(FstSites, "~/Desktop/Scripts/Data/FST/8Feb22_5pop_NOLIST--Fst-Sites.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
 
 
 # Creates Fst-Fst matrix ~
@@ -45,7 +47,8 @@ for (i in 1:nrow(Fst)) {
 
 
 # Writes Fst-Fst matrix ~
-write.table(Fst_Fst, "Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst-Fst.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+#write.table(Fst_Fst, "~/Desktop/Scripts/Data/FST/8Feb22_5pop_list--Fst-Fst.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(Fst_Fst, "~/Desktop/Scripts/Data/FST/8Feb22_5pop_NOLIST--Fst-Fst.txt", sep = "\t", row.names = TRUE, col.names = TRUE)
 
 
 # Gets Fst midpoint ~
@@ -70,12 +73,12 @@ Fst_Plot <-
 ggplot(data = Fst, aes(x = Pop1, y = Pop2, fill = Weighted)) +
   geom_tile(color = "#ffffff", lwd = 1.5, linetype = 1) +
   coord_fixed() +
-  geom_text(aes(label = Weighted), color = "#000000", size = 7) +
-  scale_fill_gradient2(low = "#80b1d3", mid = "#fccde5", high = "#fb8072",
+  geom_text(aes(label = round(Weighted,digits =3)), color = "#ffffff", size = 7) +
+  scale_fill_gradient2(low = "#ffffff", mid = "#D1A38A", high = "#000000",
                        midpoint = Fstmiddle, name = Fst.label,
-                       breaks = c(0.02,  0.06, 0.10,  0.14, 0.18, 0.22),
-                       labels = c("0.02", "0.06", "0.10", "0.14", "0.18", "0.22"),
-                       limits = c(0, .22)) +
+                       breaks = c(0.01,  0.05, 0.10,  0.15, 0.2, 0.25),
+                       labels = c("0.01", "0.05", "0.10", "0.15", "0.2", "0.25"),
+                       limits = c(0, .25)) +
   scale_x_discrete(expand = c(0,0))+
   scale_y_discrete(expand = c(0,0), position = "right")+
   theme(panel.background = element_rect(fill = "#ffffff"),
@@ -96,7 +99,9 @@ ggplot(data = Fst, aes(x = Pop1, y = Pop2, fill = Weighted)) +
         legend.text = element_text(colour = "#000000", size = 12, face="bold"))
 
 # Saves plot ~
-ggsave(Fst_Plot, file = "BSG_Turbot_Ind66--Fst.pdf",
+#ggsave(Fst_Plot, file = "~/Desktop/Scripts/Flat_oysters/04_local_R/03_results/FST/Paiwise_fst_LIST_trialdataset_feb22.pdf",
+#       device = cairo_pdf, scale = 1, width = 12, height = 12, dpi = 600)
+ggsave(Fst_Plot, file = "~/Desktop/Scripts/Flat_oysters/04_local_R/03_results/FST/Paiwise_fst_NOLIST_trialdataset_feb22.pdf",
        device = cairo_pdf, scale = 1, width = 12, height = 12, dpi = 600)
 
 
