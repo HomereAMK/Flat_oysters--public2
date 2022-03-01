151180a7256d141467267c573d6d86d7d57b0700
HomereAMK <hmon@aqua.dtu.dk>
2022-03-01 08:30:54 -0500

    pca inversion

--- 00_scripts/04_Pca_in_inversion.sh
+++ 00_scripts/04_Pca_in_inversion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #PBS -d /home/projects/dp_00007/people/hmon/Flat_oysters
 #PBS -W group_list=dp_00007 -A dp_00007
-#PBS -N PcaInInversion
-#PBS -e PcaInInversion.err
-#PBS -o PcaInInversion.out
+#PBS -N PcaInInversionBatch3
+#PBS -e PcaInInversionBatch3.err
+#PBS -o PcaInInversionBatch3.out
 #PBS -l nodes=2:ppn=36:fatnode
 #PBS -l walltime=600:00:00
 #PBS -l mem=1300gb
@@ -31,7 +31,7 @@ angsd sites index /home/projects/dp_00007/people/hmon/Flat_oysters/05_inversions
 	#script
 	cd /home/projects/dp_00007/people/hmon/Flat_oysters
 	angsd \
-	-b $BAMLIST -ref $REF -out  $OUTPUTDIR/${query}_inv \
+	-b $BAMLIST -ref $REF -out  $OUTPUTDIR/batch3_${query}_inv -r ${query}: \
 	-remove_bads 1 -uniqueOnly 1 -baq 1 -C 50 -minMapQ 20 -minQ 20 -setMaxDepth 1000 -MinMaf 0.015 -SNP_pval 1e-6 -postCutoff 0.95  \
 	-GL 2 -doMajorMinor 4 -doMaf 1 -doCounts 1 -doGlf 2 -doPost 2 -doGeno 2 -dumpCounts 2 -doHaploCall 1 -doIBS 1 -doDepth 1 \
 	-doCov 1 -makeMatrix 1 -P 12 -sites /home/projects/dp_00007/people/hmon/Flat_oysters/05_inversions/${query}_inv/${query}_inversion_snps.txt
---
+++ 04_local_R/00_scripts/GEO_FlatOyster_Feb22--Map.R
@@ -0,0 +1,217 @@
+### The BEGINNING ~~~~~
+##
+# ~ Creates Map | By George PACHECO with later modification by Homère Alves Monteiro
+rm(list=ls(all.names = TRUE))
+
+
+
+
+# Loads required packages ~
+pacman::p_load(rnaturalearth, rnaturalearthdata, rgeos, sf, ggspatial, tidyverse, ggrepel, extrafont, cowplot)
+#install.packages("sf", configure.args = "--with-proj-lib=/usr/local/lib/")
+
+# Imports extra fonts ~
+loadfonts(device = "win", quiet = TRUE)
+
+
+# Imports .shp files ~
+Global <- ne_countries(scale = 'medium', returnclass = 'sf')
+
+
+# Loads coordinates ~
+Coords_Global <- read.csv("~/Desktop/Scripts/Shucking/01_infofiles/Master_list_pop.csv", header = TRUE, encoding = "UTF-8")
+Coords_Global$lon <- as.numeric(Coords_Global$lon)
+Coords_Global$lat <- as.numeric(Coords_Global$lat)
+
+
+
+# Transforms coordinates ~
+Coords_Global_sf <- st_as_sf(Coords_Global, coords = c("lon", "lat"), crs = 4326)
+
+
+# Reorganises the data ~
+#Coords_Global_sf$Class_Article <- factor(Coords_Global_sf$Class_Article, levels=c(
+                               "Remote Localities Within Natural Range",
+                               "Urban Localities Within Natural Range",
+                               "Localities Outside Natural Range",
+                               "Captive Populations"))
+Coords_Global <- Coords_Global %>% filter(Tag != "HVAD")
+Coords_Global <- Coords_Global %>% filter(Tag != "GASO")
+Coords_Global <- Coords_Global %>% filter(Tag != "KLEV")
+Coords_Global <- Coords_Global %>% filter(Tag != "BOVA")
+Coords_Global <- Coords_Global %>% filter(Tag != "VADE")
+Coords_Global <- Coords_Global %>% filter(Tag != "HAVS")
+Coords_Global <- Coords_Global %>% filter(Tag != "Havs")
+Coords_Global <- Coords_Global %>% filter(Tag != "SVAL")
+Coords_Global <- Coords_Global %>% filter(Tag != "LILL")
+Coords_Global <- Coords_Global %>% filter(Tag != "SYDK")
+Coords_Global <- Coords_Global %>% filter(Tag != "USAM")
+
+Coords_Global$Tag <- factor(Coords_Global$Tag, ordered = T,
+                          levels = c("MOLU", "ZECE", "CRES",
+                                     "ORIS","CORS", "PONT",  "RIAE",
+                                     "MORL",
+                                     "USAM",
+                                     "TOLL", "COLN", "BARR", 
+                                     "TRAL", "CLEW",
+                                     "RYAN", "NELL",
+                                     "GREV", "WADD", 
+                                     "FURI", "NISS","LOGS","VENO", "HALS", "THIS",
+                                     "HVAD", "KALV",  "HFJO", "RAMS", "ORNE", "HYPP",
+                                     "LANG", "BUNN", "DOLV", "HAUG", "HAFR",  
+                                     "INNE","VAGS", "AGAB", "OSTR"))
+
+
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "HVAD")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "GASO")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "KLEV")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "BOVA")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "VADE")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "HAVS")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "Havs")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "SVAL")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "LILL")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "SYDK")
+Coords_Global_sf <- Coords_Global_sf %>% filter(Tag != "USAM")
+
+Coords_Global_sf$Tag <- factor(Coords_Global_sf$Tag, ordered = T,
+                               levels = c("MOLU", "ZECE", "CRES",
+                                          "ORIS","CORS", "PONT",  "RIAE",
+                                          "MORL",
+                                          "USAM",
+                                          "TOLL", "COLN", "BARR", 
+                                          "TRAL", "CLEW",
+                                          "RYAN", "NELL",
+                                          "GREV", "WADD", 
+                                          "FURI", "NISS","LOGS","VENO", "HALS", "THIS",
+                                          "HVAD", "KALV",  "HFJO", "RAMS", "ORNE", "HYPP",
+                                          "LANG", "BUNN", "DOLV", "HAUG", "HAFR",  
+                                          "INNE","VAGS", "AGAB", "OSTR"))
+
+
+# Creates the base maps ~ limits = c(-12, 20, 36, 73),
+
+# Map1 - Global ~
+Map1 <-
+ ggplot() +
+  geom_sf(data = Global, fill = "#fff5f0", color = "black") +
+  geom_sf(data = Coords_Global_sf, aes(fill = Tag), size = 3, show.legend = "point", shape = 21, colour = "black") +
+  coord_sf(xlim = c(-25, 35), ylim = c(36, 73), expand = FALSE) +
+  geom_label_repel(data = Coords_Global, size = 2, seed = 10, min.segment.length = 0, force = 5, segment.curvature = 1,
+                   nudge_x = 0, nudge_y = 0, max.overlaps = Inf, fontface = "bold", colour = "black",
+                   aes(x = lon, y = lat, label = Tag,
+                   fill = Tag, family = "Helvetica"), alpha = 0.9, show.legend = FALSE) +
+  scale_fill_manual(values = c("#A02353", "#A02353", "#A02353",
+                               "#AD5B35",
+                               "#ad7358",
+                               "#CC480C",  "#CC480C",
+                               "#969696",
+                               "#000000",
+                               "#D38C89", "#D38C89", "#D38C89",
+                               "#C89AD1", "#C89AD1",
+                               "#7210A0", "#7210A0",
+                               "#91BD96", "#91BD96",
+                               "#02630C", "#02630C","#02630C","#02630C", "#02630C", "#02630C",
+                               "#45D1F7", "#45D1F7", "#45D1F7", "#45D1F7",  "#45D1F7","#45D1F7",
+                               "#588cad", "#588cad", "#588cad", "#588cad", "#588cad",
+                               "#240377", "#240377", "#240377", "#240377"), drop = FALSE) +
+  scale_colour_manual(values = c("#A02353", "#A02353", "#A02353",
+                                 "#AD5B35",
+                                 "#ad7358",
+                                 "#CC480C",  "#CC480C",
+                                 "#969696",
+                                 "#000000",
+                                 "#D38C89", "#D38C89", "#D38C89",
+                                 "#C89AD1", "#C89AD1",
+                                 "#7210A0", "#7210A0",
+                                 "#91BD96", "#91BD96",
+                                 "#02630C", "#02630C","#02630C","#02630C", "#02630C", "#02630C",
+                                 "#45D1F7", "#45D1F7", "#45D1F7", "#45D1F7",  "#45D1F7","#45D1F7",
+                                 "#588cad", "#588cad", "#588cad", "#588cad", "#588cad",
+                                 "#240377", "#240377", "#240377", "#240377"), drop = FALSE) +
+  scale_x_continuous(breaks = seq(-120, 120, by = 25)) +
+  scale_y_continuous(breaks = seq(-20, 70, by = 25)) +
+  annotation_north_arrow(location = "tl", which_north = "false", style = north_arrow_fancy_orienteering,
+                         pad_x = unit(0.25, "in"), pad_y = unit(6.5, "in")) +
+  annotation_scale(location = 'br', line_width = 2, text_cex = 1.35, style = "ticks") +
+  #annotate("rect", xmin = 76.9, xmax = 84, ymin = 3.25, ymax = 12, fill = "#f46d43",  alpha = 0.2, color = "#a50026", linetype = "dotdash") +
+  #annotate("rect", xmin = -9, xmax = -4.5, ymin = 59.5, ymax = 64.5, fill = "#f46d43",  alpha = 0.2, color = "#a50026", linetype = "dotdash") +
+  theme(panel.background = element_rect(fill = "#f7fbff"),
+        panel.border = element_rect(colour = "black", size = 0.5, fill = NA),
+        panel.grid.major = element_line(color = "#d9d9d9", linetype = "dashed", size = 0.00005),
+        plot.margin = margin(t = 0.005, b = 0.005, r = 0.2, l = 0.2, unit = "cm"),
+        legend.background = element_rect(fill = "#c6dbef", size = 0.15, color = "#5e5e5e", linetype = "dotted"),
+        legend.key = element_rect(fill = "#c6dbef"))+
+        #legend.position = c(0.10000, 0.18250)) +
+  theme(axis.text.x = element_text(color = "black", size = 13, family = "Helvetica"),
+        axis.text.y = element_text(color = "black", size = 13, family = "Helvetica"),
+        axis.title = element_blank()) +
+  theme(axis.ticks = element_line(color ="black", size = 0.5)) +
+  guides(fill = guide_legend(title = "Populations", title.theme = element_text(size = 10.5, face = "bold", family = "Helvetica"),
+                             label.theme = element_text(size = 8, face = "italic", family = "Helvetica"),
+                             override.aes = list(size = 3, alpha = 0.9)), colour = "none")
+
+
+ggsave(Map1, file = "~/Desktop/Scripts/Flat_oysters/04_local_R/03_results/PCA/Global_map.pdf", device = cairo_pdf, width = 26, height = 13, scale = 0.8, limitsize = FALSE, dpi = 1000)
+
+# Map1 - Global ~
+quartz()
+
+Map1 <-
+  ggplot() +
+  geom_sf(data = Global, fill = "#fff5f0", color = "black") +
+  geom_sf(data = Coords_Global_sf, aes(fill = Tag), size = 2, show.legend = "point", shape = 21, colour = "black") +
+  coord_sf(xlim = c(-20, 30), ylim = c(36, 73), expand = FALSE) +
+  geom_label_repel(data = Coords_Global, size = 1, seed = 10, min.segment.length = 0, force = 25, segment.curvature = 1,
+                   nudge_x = 0, nudge_y = 0, max.overlaps = Inf, fontface = "bold", colour = "black",
+                   aes(x = lon, y = lat, label = Tag,
+                       ), alpha = 0.9, show.legend = FALSE) +
+  scale_fill_manual(values = c("black","#A02353", "#A02353", "#A02353",
+                               "#AD5B35",
+                               "#ad7358",
+                               "#CC480C",  "#CC480C",
+                               "#969696",
+                               "#000000",
+                               "#D38C89", "#D38C89", "#D38C89",
+                               "#C89AD1", "#C89AD1",
+                               "#7210A0", "#7210A0",
+                               "#91BD96", "#91BD96",
+                               "#02630C", "#02630C","#02630C","#02630C", "#02630C", "#02630C",
+                               "#45D1F7", "#45D1F7", "#45D1F7", "#45D1F7",  "#45D1F7",
+                               "#588cad", "#588cad", "#588cad", "#588cad", "#588cad",
+                               "#240377", "#240377", "#240377", "#240377"), drop = FALSE) +
+  scale_colour_manual(values = c("black","#A02353", "#A02353", "#A02353",
+                                 "#AD5B35",
+                                 "#ad7358",
+                                 "#CC480C",  "#CC480C",
+                                 "#969696",
+                                 "#000000",
+                                 "#D38C89", "#D38C89", "#D38C89",
+                                 "#C89AD1", "#C89AD1",
+                                 "#7210A0", "#7210A0",
+                                 "#91BD96", "#91BD96",
+                                 "#02630C", "#02630C","#02630C","#02630C", "#02630C", "#02630C",
+                                 "#45D1F7", "#45D1F7", "#45D1F7", "#45D1F7",  "#45D1F7",
+                                 "#588cad", "#588cad", "#588cad", "#588cad", "#588cad",
+                                 "#240377", "#240377", "#240377", "#240377"), drop = FALSE) +
+  scale_x_continuous(breaks = seq(-120, 120, by = 25)) +
+  scale_y_continuous(breaks = seq(-20, 70, by = 25)) +
+  annotation_north_arrow(location = "br", which_north = "false", style = north_arrow_fancy_orienteering,
+                         pad_x = unit(0.25, "in"), pad_y = unit(6.5, "in")) +
+  annotation_scale(location = 'br', line_width = 2, text_cex = 1.35, style = "ticks") +
+  annotate("rect", xmin = 76.9, xmax = 84, ymin = 3.25, ymax = 12, fill = "#f46d43",  alpha = 0.2, color = "#a50026", linetype = "dotdash") +
+  annotate("rect", xmin = -9, xmax = -4.5, ymin = 59.5, ymax = 64.5, fill = "#f46d43",  alpha = 0.2, color = "#a50026", linetype = "dotdash") +
+  theme(panel.background = element_rect(fill = "#f7fbff"),
+        panel.border = element_rect(colour = "black", size = 0.5, fill = NA),
+        panel.grid.major = element_line(color = "#d9d9d9", linetype = "dashed", size = 0.00005),
+        plot.margin = margin(t = 0.005, b = 0.005, r = 0.2, l = 0.2, unit = "cm"),
+        legend.background = element_rect(fill = "#c6dbef", size = 0.15, color = "#5e5e5e", linetype = "dotted"),
+        legend.key = element_rect(fill = "#c6dbef"),
+        legend.position = c(0.10000, 0.18250)) +
+  theme(axis.text.x = element_text(color = "black", size = 13, family = "Helvetica"),
+        axis.text.y = element_text(color = "black", size = 13, family = "Helvetica"),
+        axis.title = element_blank()) +
+  theme(axis.ticks = element_line(color ="black", size = 0.5)) +
+  guides(fill = guide_legend(title = "Biological Status", title.theme = element_text(size = 10.5, face = "bold", family = "Helvetica"),
+                             label.theme = element_text(size = 8, face = "italic", family = "Helvetica"),
+                             override.aes = list(size = 3, alpha = 0.9)), colour = "none")
